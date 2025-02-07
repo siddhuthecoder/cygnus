1c4a6770eec76c0862cb4900d321eb3b3cf53177
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2025-02-07 12:06:04 +0530

    About page updated

--- src/pages/about/homeabout.jsx
+++ src/pages/about/homeabout.jsx
@@ -35,13 +35,13 @@ function HomeAbout(){
                     <div className="w-full h-auto px-4 py-2 flex flex-col justify-center items-center about-border">
                         <div className="w-full h-[300px] md:h-[460px] p-2 grid grid-flow-col grid-cols-12 grid-rows-12 gap-2 ">
                             <div className="col-span-7 row-span-7  rounded-md">
-                            <img className="w-full h-full object-cover rounded-md" alt="image" src={"https://res.cloudinary.com/db552uxtr/image/upload/v1738871021/cygnus_about_n8tpyi.jpg"} />
+                            <img className="w-full h-full object-cover rounded-md" alt="image" src={"https://res.cloudinary.com/db552uxtr/image/upload/v1738909954/about_1_nmuxq4.jpg"} />
                             </div>
                             <div className="col-span-7 row-span-5 rounded-md">
                                 <img className="w-full h-full object-cover rounded-md " alt="image" src={"https://res.cloudinary.com/db552uxtr/image/upload/v1738871180/about2_jxzxkt.jpg"} />
                             </div>
                             <div className="col-span-5 row-span-5 rounded-md">
-                            <img className="w-full h-full object-cover rounded-md" alt="image" src={"https://res.cloudinary.com/db552uxtr/image/upload/v1738794576/studentmitra_zmcbwt.jpg"} />
+                            <img className="w-full h-full object-cover rounded-md" alt="image" src={"https://res.cloudinary.com/db552uxtr/image/upload/v1738909959/about_2_tkntt3.jpg"} />
                             </div>
                             <div className="col-span-5 row-span-7 rounded-md">
                             <img className="w-full h-full object-cover rounded-md object-center" alt="image" src={"https://res.cloudinary.com/db552uxtr/image/upload/v1738788967/IMG-20250206-WA0007_kdweyp.jpg"} />
