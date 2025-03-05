8ba4fc55f74f41fa4710a3516a215ec14c2ddc7b
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2025-03-05 22:35:33 +0530

    backend added

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,5 @@
 import { meta } from "@eslint/js";
 
-const BASE_URL= import.meta.env.VITE_BACKEND_URL
+// const BASE_URL= import.meta.env.VITE_BACKEND_URL
+const BASE_URL="https://cygnus-xqix.onrender.com"
 export default BASE_URL;
